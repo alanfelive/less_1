91691fa8b51a3d2746de8f1dee61843e1f7f6c8e
Aleksandr Fedotov <al_fed@live.ru>
2024-11-13 06:56:57 +0400

    ДЗ 8 исправление E19-E20

--- home8/task_E19.c
+++ home8/task_E19.c
@@ -1,28 +1,33 @@
 #include <stdio.h>
 
-int covertMaxNum (int n)
+void arrayPrint (int print_arr[], int s)
+{
+    for (int i=0; i < s; i++)
+        printf("%d ", print_arr[i]);
+}
+
+int numToArray (int n)
 {
     int k = 0;
     int n_copy = n;
     while (n_copy)
     {
-        k ++;
+        k++;
         n_copy /= 10;
     }
-    int arr[k];
+    int print_arr[k];
     for (int i = k - 1; i >=0; i--)
     {
-        arr[i] = n % 10;
+        print_arr[i] = n % 10;
         n /= 10;
     }
-    for (int i = 0; i < k; i++)
-        printf("%d ", arr[i]);
+    arrayPrint(print_arr, k);
 }
 
 int main(void)
 {
     int n;
     scanf("%d", &n);
-    covertMaxNum(n);
+    numToArray(n);
     return 0;
 }
--- home8/task_E20.c
+++ home8/task_E20.c
@@ -1,28 +1,60 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int covertMaxNum (int n)
+void arrayPrint (int arr[], int s)
 {
-    int k = 0;
-    int n_copy = n;
-    while (n_copy)
+    for (int i=0; i < s; i++)
+        printf("%d", arr[i]);
+}
+
+int arraySort(int arr[], int s)
+{
+    int temp;
+    int flag;
+    for (int i = s - 1; i >= 0; i--)
     {
-        k ++;
-        n_copy /= 10;
+        flag = 1;
+        for (int j = 0; j < i; j++)
+        {
+            if (arr[j + 1] > arr[j])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                flag = 0;
+            }
+        }
+        if (flag)
+            break;
     }
-    int arr[k];
+}
+
+int numToArray (int arr[], int n, int k)
+{
     for (int i = k - 1; i >=0; i--)
     {
         arr[i] = n % 10;
         n /= 10;
     }
-    for (int i = 0; i < k; i++)
-        printf("%d ", arr[i]);
 }
+
 
 int main(void)
 {
     int n;
+    int n_copy;
+    int k = 0;
     scanf("%d", &n);
-    covertMaxNum(n);
+    n_copy = n;
+    while (n_copy)
+    {
+        k++;
+        n_copy /= 10;
+    }
+    int *arr;
+    arr=(int*)malloc(k * sizeof(int));
+    numToArray(arr, n, k);
+    arraySort(arr, k);
+    arrayPrint(arr, k);
     return 0;
 }
